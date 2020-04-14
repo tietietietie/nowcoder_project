d1876da7cf59e89e42be17576ae5f26cf669f755
ZhangTie <zhangtie@hust.edu.cn>
2020-04-14 21:19:56 +0800

    学习如何调试项目

---
+++ workspace/community/src/test/java/com/nowcoder/community/LoggerTests.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger() {
+        System.out.println(logger.getName());
+
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+
+}
